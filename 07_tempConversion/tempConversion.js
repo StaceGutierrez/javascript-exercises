9175ae13c93b84a2dc1933aad9703e7b1c36bd8a
StaceGutierrez <26301078+StaceGutierrez@users.noreply.github.com>
2023-09-10 18:55:48 -0500

    Apply conditions to temperature functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = ((5/9) * (f-32) * 10);
+    return Math.round(celsius) / 10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let farhrenheit = (((c * 1.8) + 32)) *10;
+    return Math.round(farhrenheit) / 10;
 };
 
 // Do not edit below this line
