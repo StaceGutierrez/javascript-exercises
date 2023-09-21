015df31b1f4bf0b5c5ff425a8477c8d0cd42b638
StaceGutierrez <26301078+StaceGutierrez@users.noreply.github.com>
2023-09-21 12:58:57 -0500

    Addarguments to reverse string and test function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const str = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const newStr = str.split("").reverse().join("");
 
+    if (str === newStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
