046a6f9aa11d95ca7cb74ad529d6cf1e8bcc04d0
StaceGutierrez <26301078+StaceGutierrez@users.noreply.github.com>
2023-09-21 18:49:07 -0500

    Add arguments for fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n > 0) {
+        let a = 0
+        let b = 1
+        let c = 1
 
+        let fibCounter = 1;
+
+        while (fibCounter < n) {
+            c = a + b
+
+            fibCounter++
+            a = b 
+            b = c
+            
+            
+        }
+        return c
+    } else {
+        return "OOPS"
+    }
+
+
+    
 };
 
 // Do not edit below this line
